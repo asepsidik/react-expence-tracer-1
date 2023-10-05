5ebb5dcb1c3b0b4e9f287620bc9a5a51f1ccc657
asepsirs <asepsirs@gmail.com>
2023-10-05 08:46:33 +0700

    menambahkan folder Components, create file Post.jsx dan edit file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Post from "./components/Post";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1>Hello World!</h1>;
+  // return <h1>Hello World!</h1>;
+  return <Post />;
 }
 
 export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,6 @@
+function Post() {
+  const Name = ["Asep", "Sidik"];
+  return <h1>{Name[0]}</h1>;
+}
+
+export default Post;
